679d0e83a20a00eae647a283d8d841b052f6d07c
changwhaj <changwha.jeong@gmail.com>
2021-08-11 23:37:11 +0900

    remove base

--- exam/aws/MakeSAP.py
+++ exam/aws/MakeSAP.py
@@ -129,7 +129,7 @@ def save_html(driver, fname):
 if __name__ == "__main__":
     driver = webdriver.Chrome('c:/temp/chromedriver.exe')
 
-    for i in reversed(range(len(ArraySAP))[450:551]):
+    for i in reversed(range(len(ArraySAP))[350:451]):
         qSAP = ArraySAP[i]
         qid = qSAP.get('QID')
         did = qSAP.get('DID')
