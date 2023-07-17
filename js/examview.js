e3e6e3e702ea2f4ead1f68428970257e892fc044
changwhaj <changwha.jeong@gmail.com>
2023-07-17 23:25:31 +0900

    Update examview.js

--- js/examview.js
+++ js/examview.js
@@ -14,3 +14,22 @@
         $(this).parent('.question-body').find('.correct-hidden').removeClass('correct-choice');
         $(this).addClass('d-none');
     });
+
+    function set_voting_configuration_by_question(question_jquery_object) {
+        // Voted comments are only for MCQ questions, this function has nothing to do on other questions
+        if (!is_question_mcq(question_jquery_object))
+            return;
+
+        let choice_limit = question_jquery_object.find(".correct-answer").text().trim().length;
+        let choices_elements = question_jquery_object.find(".multi-choice-letter");
+
+        let choice_letters = "";
+        choices_elements.each(function() {
+            choice_letters += $(this).text().trim()[0];
+        });
+
+        let question_id = question_jquery_object.find(".question-body").addBack('.question-body').data("id");
+        let discussion_object = getDiscussionObjectByQuestionId(question_id);
+
+        set_voted_comment_config(discussion_object, choice_letters, choice_limit);
+    }
