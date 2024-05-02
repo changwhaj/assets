db93204fd567d3f295d7be06a3979b9194f1c1da
changwhaj <changwha.jeong@gmail.com>
2024-05-02 20:53:37 +0900

    Exam Refresh on 20240502

--- exam/MakeExam.py
+++ exam/MakeExam.py
@@ -850,7 +850,7 @@ if __name__ == "__main__":
     
     DISCUSS = 'AmazonDiscuss.txt'
     FORUM_NAME = 'amazon'
-    refresh_from_forum(DISCUSS, FORUM_NAME, 29)
+    refresh_from_forum(DISCUSS, FORUM_NAME, 1)
     
     DISCUSS = 'IsacaDiscuss.txt'
     FORUM_NAME = 'isaca'
