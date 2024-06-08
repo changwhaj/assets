dbb1740d326075f625a5df547f5c13ed12dfbea9
changwhaj <changwha.jeong@gmail.com>
2024-06-08 12:08:17 +0900

    Exam Refresh on 20240608

--- exam/MakeExam.py
+++ exam/MakeExam.py
@@ -497,12 +497,12 @@ def make_filename(qtitle, qid, dataid):
         #     "qlength": 359,
         #     "first_id": 807243,
         # },
-                # {
-                #     "qtitle": "Exam AWS Certified Developer - Associate DVA-C02 topic 1",
-                #     "prefname": "aws/DVA_C02/DVA2-Q",
-                #     "qlength": 357,
-                #     "first_id": 874573,
-                # },
+        {
+            "qtitle": "Exam AWS Certified Developer - Associate DVA-C02 topic 1",
+            "prefname": "aws/DVA_C02/DVA2-Q",
+            "qlength": 357,
+            "first_id": 874573,
+        },
                 # {
                 #     "qtitle": "Exam AWS Certified DevOps Engineer - Professional DOP-C02 topic 1",
                 #     "prefname": "aws/DOP_C02/DOP2-Q",
@@ -521,18 +521,18 @@ def make_filename(qtitle, qid, dataid):
         #     "qlength": 173,
         #     "first_id": 897420,
         # },
-                # {
-                #     "qtitle": "Exam AWS Certified Solutions Architect - Associate SAA-C03 topic 1",
-                #     "prefname": "aws/SAA_C03/SAA3-Q",
-                #     "qlength": 904,
-                #     "first_id": 839758,
-                # },
         {
-            "qtitle": "Exam AWS Certified Solutions Architect - Professional SAP-C02 topic 1",
-            "prefname": "aws/SAP_C02/SAP2-Q",
-            "qlength": 481,
-            "first_id": 856116,
+            "qtitle": "Exam AWS Certified Solutions Architect - Associate SAA-C03 topic 1",
+            "prefname": "aws/SAA_C03/SAA3-Q",
+            "qlength": 904,
+            "first_id": 839758,
         },
+                # {
+                #     "qtitle": "Exam AWS Certified Solutions Architect - Professional SAP-C02 topic 1",
+                #     "prefname": "aws/SAP_C02/SAP2-Q",
+                #     "qlength": 481,
+                #     "first_id": 856116,
+                # },
         # {
         #     "qtitle": "Exam AWS Certified SysOps Administrator - Associate topic 1",
         #     "prefname": "aws/SOA_C02/SOA2-Q",
